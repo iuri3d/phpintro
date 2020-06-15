2b2d121395f59905d2dc876d2d28e65261987cc6
Iuri Pereira <iuri.3d@gmail.com>
2020-06-15 12:27:11 +0100

    layout mainpage

--- src/includes/head.php
+++ src/includes/head.php
@@ -5,5 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>SuperApp®</title>
   <link rel="icon" href="favicon.ico" type="image/png"/>
+
+  <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
   <link rel="stylesheet" href="/styles/styles.css">
 </head>
--- src/includes/scripts.php
+++ src/includes/scripts.php
@@ -1,3 +1,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/js/all.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
 <script src="scripts/main.js"></script>
