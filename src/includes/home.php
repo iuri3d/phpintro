61882042d3dbcd7d2130172d5f001ea7e02f7a21
Iuri Pereira <iuri.3d@gmail.com>
2020-06-15 11:34:03 +0100

    ajustar navbar

--- src/includes/home.php
+++ src/includes/home.php
@@ -1,16 +1,19 @@
 <body>
     <div class="navbar">
-        <div class="links">
-            <div class="filter"></div>
-            <div class="link"><a href="/">Início</a></div>
-            <div class="link"><a href="/">Produtos</a></div>
-            <div class="link"><a href="/">Encomendas</a></div>
-        </div>
-        <div class="cart"><i class="fas fa-shopping-cart"></i></div>
-        <div class="profile">
-            <div class="profile-name">José Manuel</div>
-            <div class="profile-image"></div>
-        </div>
+        <div class="content">
+            <div class="links">
+                <div class="link"><a href="/">Início</a></div>
+                <div class="link"><a href="/">Produtos</a></div>
+                <div class="link"><a href="/">Encomendas</a></div>
+            </div>
+            <div class="user">
+                <div class="cart"><i class="fas fa-shopping-cart"></i></div>
+                <div class="profile">
+                    <div class="profile-name">José Manuel</div>
+                    <div class="profile-image"></div>
+                </div>
+            </div>
+        </div>        
     </div>
 
     <div id="home">
